256a1a0f6bf08ec28da2fff042a08c4a44ab9648
lilinormal <126519937+lilinormal@users.noreply.github.com>
2023-06-04 11:15:52 -0600

    Add files via upload

---
+++ search.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+		<script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/superfish.js"></script>
+        <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+         
+        <script type="text/javascript" src="search/search.js"></script>
+    
+  <!--[if lt IE 8]>
+      <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]>
+     
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->      
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="span12 sub-header clearfix">
+            <h1 class="brand"><a href="index.html"><span>smart</span>connect</a></h1>
+            <div class="navbar navbar_ clearfix">
+              <div class="navbar-inner">                                                 
+                <div class="nav-collapse nav-collapse_ collapse">
+                  <ul class="nav sf-menu clearfix">
+                    <li><a href="index.html">Home Page</a></li>
+                    <li class="sub-menu"><a href="index-1.html">Company</a>
+                      <ul>
+                        <li><a href="#">Welcome Message</a></li>
+                        <li class="sub-menu"><a href="#">Company Profile</a>
+                          <ul>
+                            <li><a href="#">Our Capabilities</a></li>
+                            <li><a href="#">Advantages</a></li>
+                            <li><a href="#">Work Team</a></li>
+                            <li><a href="#">Partnership</a></li>
+                            <li><a href="#">Support</a></li>
+                          </ul>  
+                        </li>
+                        <li><a href="#">Our History</a></li>
+                        <li><a href="#">Testimonials</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="index-2.html">Services</a></li>
+                    <li><a href="index-3.html">Projects</a></li>
+                    <li><a href="index-4.html">Support</a></li>
+                    <li><a href="index-5.html">Contacts</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>  
+           </div> 
+         </div>
+       </div>    
+    </header>
+    <!--==============================content=================================-->
+    <div id="content">     
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+               <h2 class="h2-pad">Search result:</h2>
+               <div id="search-results"></div>
+          </div>
+        </div>
+      </div>
+  </div>    
+    <!--==============================footer=================================-->
+<footer>
+  <div class="container">
+     <div class="row">
+       <div class="span12">
+          <div class="social-icons">
+            <span>Stay Connected</span>	
+            <ul>
+               <li><a href="#" class="icon-1"></a></li>
+               <li><a href="#" class="icon-2"></a></li>
+               <li><a href="#" class="icon-3"></a></li>
+               <li><a href="#" class="icon-4"></a></li>
+           </ul>
+          </div> 
+          <span><a href="index.html" class="footer-logo"><span>smart</span>connect</a><span>&copy; 2013 | <a href="index-6.html">Privacy Policy</a></span></span>
+       </div>
+     </div>  
+  </div>
+</footer>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
